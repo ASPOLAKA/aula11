5518a1cfeab9daf9b6ebd51c0adb35e68aceb955
Angela Santos <145032472+ASPOLAKA@users.noreply.github.com>
2024-03-05 10:24:37 -0300

    Create ex008.js

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 71
+console.log(`A velocidade permitida do seu carro é ${vel} km/h`)
+if (vel > 60) {//condição simples
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
+
